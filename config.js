05463a9212a4ced3474b3fd13bfd38f91cd28d51
caloriadev2026-cell <caloria.dev2026@gmail.com>
2026-04-11 19:20:56 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const SUPABASE_URL  = 'https://htdqjrmzlvlchjxfttzf.supabase.co';
+const SUPABASE_KEY  = 'sb_publishable_Ko0EPVLdgsA_4neIWAX0dQ_MaZe3X_N';
+const FREE_LIMIT    = 10;
+const DAILY_CAL_REF = 2000;
+const ANTHROPIC_KEY = '';
+const GEMINI_KEY    = 'AIzaSyDTeBaEZxtesl_NUmZ3aI6PNtaJGzKYc7s';
