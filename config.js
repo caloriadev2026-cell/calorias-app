cba2524ddd97f95724fd7cf0531971bd8c229277
caloriadev2026-cell <caloria.dev2026@gmail.com>
2026-04-11 18:20:22 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL  = 'https://htdqjrmzlvlchjxfttzf.supabase.co';
+const SUPABASE_KEY  = 'sb_publishable_Ko0EPVLdgsA_4neIWAX0dQ_MaZe3X_N';
+const FREE_LIMIT    = 10;
+const DAILY_CAL_REF = 2000;
+const ANTHROPIC_KEY = '';
